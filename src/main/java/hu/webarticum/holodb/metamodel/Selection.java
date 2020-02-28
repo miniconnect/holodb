0b42a7f083d402f450cffbdd6a6627761534a17d
Dávid Horváth <horvath@webarticum.hu>
2020-02-28 05:10:20 +0100

    Add an idea comment to Selection

--- src/main/java/hu/webarticum/holodb/metamodel/Selection.java
+++ src/main/java/hu/webarticum/holodb/metamodel/Selection.java
@@ -2,6 +2,7 @@ package hu.webarticum.holodb.metamodel;
 
 import java.math.BigInteger;
 
+// FIXME: should this be more complex (decorable, limit etc.)
 public interface Selection extends Iterable<BigInteger> {
 
     public int size();
