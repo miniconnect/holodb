392678b74093d8af350281dd9b2bc2a3388f3c06
Dávid Horváth <horvath@webarticum.hu>
2020-06-15 21:42:52 +0200

    Add a FIXME comment

--- holodb-core/src/main/java/hu/webarticum/holodb/core/data/binrel/core/Function.java
+++ holodb-core/src/main/java/hu/webarticum/holodb/core/data/binrel/core/Function.java
@@ -2,6 +2,7 @@ package hu.webarticum.holodb.core.data.binrel.core;
 
 import java.math.BigInteger;
 
+// FIXME: is this a ValueSource<BigInteger> ?
 public interface Function {
 
     public BigInteger size();
