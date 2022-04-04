4577949ba6316e6cb68082881500cee90403ffa4
Dávid Horváth <horvath@webarticum.hu>
2022-04-04 04:25:47 +0200

    Add skeleton of HoloSimpleTableIndex and some TODOs in Index

--- projects/core/src/main/java/hu/webarticum/holodb/core/data/source/Index.java
+++ projects/core/src/main/java/hu/webarticum/holodb/core/data/source/Index.java
@@ -2,6 +2,10 @@ package hu.webarticum.holodb.core.data.source;
 
 import hu.webarticum.holodb.core.data.selection.Selection;
 
+// TODO: make this similar-to/compatible-with TableIndex
+// TODO: create a basic/general/abstract 'Searchable' (or so) interface in miniconnect
+// TODO: make parameter non-generic (Object)
+// FIXME: Selection vs. TableSelection
 public interface Index<T> {
 
     public default Selection find(T value) {
