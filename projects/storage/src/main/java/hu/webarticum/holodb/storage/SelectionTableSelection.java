3b864077d735a0cd6faf9b3300b272d443e35395
Dávid Horváth <horvath@webarticum.hu>
2022-04-10 04:32:27 +0200

    Eliminate TableSelectionEntry

--- projects/storage/src/main/java/hu/webarticum/holodb/storage/SelectionTableSelection.java
+++ projects/storage/src/main/java/hu/webarticum/holodb/storage/SelectionTableSelection.java
@@ -2,12 +2,10 @@ package hu.webarticum.holodb.storage;
 
 import java.math.BigInteger;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 import hu.webarticum.holodb.core.data.selection.Selection;
 import hu.webarticum.miniconnect.rdmsframework.storage.OrderKey;
 import hu.webarticum.miniconnect.rdmsframework.storage.TableSelection;
-import hu.webarticum.miniconnect.rdmsframework.storage.TableSelectionEntry;
 
 public class SelectionTableSelection implements TableSelection {
     
@@ -41,8 +39,8 @@ public class SelectionTableSelection implements TableSelection {
     }
 
     @Override
-    public Iterator<TableSelectionEntry> iterator() {
-        return new SelectionTableSelectionIterator();
+    public Iterator<BigInteger> iterator() {
+        return selection.iterator();
     }
 
     @Override
@@ -58,45 +56,6 @@ public class SelectionTableSelection implements TableSelection {
     @Override
     public TableSelection reversed() {
         return new SelectionTableSelection(selection, ascOrderKey, descOrderKey, !ascOrder);
-    }
-    
-    
-    private class SelectionTableSelectionIterator implements Iterator<TableSelectionEntry> {
-
-        private final BigInteger size = selection.size();
-        
-        private BigInteger nextOrderIndex = BigInteger.ZERO;
-        
-        
-        @Override
-        public boolean hasNext() {
-            return nextOrderIndex.compareTo(size) < 0;
-        }
-
-        @Override
-        public TableSelectionEntry next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            
-            BigInteger selectionIndex;
-            OrderKey orderKey;
-            if (ascOrder) {
-                selectionIndex = nextOrderIndex;
-                orderKey = ascOrderKey;
-            } else {
-                selectionIndex = size.subtract(BigInteger.ONE).subtract(nextOrderIndex);
-                orderKey = descOrderKey;
-            }
-            
-            BigInteger rowIndex = selection.at(selectionIndex);
-            TableSelectionEntry entry = new TableSelectionEntry(orderKey, rowIndex, nextOrderIndex);
-            
-            nextOrderIndex = nextOrderIndex.add(BigInteger.ONE);
-            
-            return entry;
-        }
-        
     }
     
 }
