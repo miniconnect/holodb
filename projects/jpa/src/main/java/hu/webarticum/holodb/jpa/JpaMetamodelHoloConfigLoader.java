115168b0e4a9006f7294f59d1907907405413571
Dávid Horváth <horvath@webarticum.hu>
2022-08-22 21:37:10 +0200

    Add jpa project

---
+++ projects/jpa/src/main/java/hu/webarticum/holodb/jpa/JpaMetamodelHoloConfigLoader.java
@@ -0,0 +1,173 @@
+package hu.webarticum.holodb.jpa;
+
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.persistence.metamodel.Attribute;
+import javax.persistence.metamodel.EntityType;
+import javax.persistence.metamodel.Metamodel;
+import javax.persistence.metamodel.Type;
+
+import org.hibernate.metamodel.internal.MetamodelImpl;
+import org.hibernate.persister.entity.SingleTableEntityPersister;
+
+import hu.webarticum.holodb.app.config.HoloConfig;
+import hu.webarticum.holodb.app.config.HoloConfigColumn;
+import hu.webarticum.holodb.app.config.HoloConfigSchema;
+import hu.webarticum.holodb.app.config.HoloConfigTable;
+import hu.webarticum.miniconnect.lang.ImmutableList;
+import hu.webarticum.holodb.app.config.HoloConfigColumn.ColumnMode;
+
+public class JpaMetamodelHoloConfigLoader {
+    
+    private final Pattern getMethodPattern = Pattern.compile("^get([A-Z])(.*)$");
+    
+
+    public HoloConfig load(Metamodel metamodel, String selectedSchemaName, BigInteger seed) {
+        Map<String, List<HoloConfigTable>> tablesBySchema = new TreeMap<>();
+        for (EntityType<?> entityType : metamodel.getEntities()) {
+            addTable(tablesBySchema, metamodel, selectedSchemaName, entityType);
+        }
+        List<HoloConfigSchema> schemas = new ArrayList<>(tablesBySchema.size());
+        for (Map.Entry<String, List<HoloConfigTable>> entry : tablesBySchema.entrySet()) {
+            String schemaName = entry.getKey();
+            List<HoloConfigTable> tables = entry.getValue();
+            schemas.add(new HoloConfigSchema(schemaName, tables));
+        }
+        return new HoloConfig(seed, schemas);
+    }
+
+    private void addTable(
+            Map<String, List<HoloConfigTable>> tablesBySchema,
+            Metamodel metamodel,
+            String selectedSchemaName,
+            EntityType<?> entityType) {
+        String schemaName = selectedSchemaName; // TODO
+        String tableName;
+        boolean writeable = true; // TODO
+        BigInteger tableSize = BigInteger.valueOf(100L); // TODO
+        Class<?> entityClazz = entityType.getJavaType();
+        if (metamodel instanceof MetamodelImpl) {
+            MetamodelImpl hibernateMetamodel = (MetamodelImpl) metamodel;
+            SingleTableEntityPersister entityPersister =
+                    (SingleTableEntityPersister) hibernateMetamodel.entityPersister(entityClazz);
+            tableName = entityPersister.getTableName();
+        } else {
+            Table tableAnnotation = entityClazz.getAnnotation(Table.class);
+            if (tableAnnotation != null && !tableAnnotation.name().isEmpty()) {
+                tableName = tableAnnotation.name();
+            } else {
+                tableName = entityClazz.getSimpleName();
+            }
+        }
+        String idAttributeName = null;
+        Type<?> idType = entityType.getIdType();
+        if (idType != null) {
+            idAttributeName = entityType.getId(entityType.getIdType().getJavaType()).getName();
+        }
+        List<Attribute<?, ?>> attributes = new ArrayList<>(entityType.getAttributes());
+        sortAttributes(entityClazz, attributes);
+        List<HoloConfigColumn> columns = new ArrayList<>(attributes.size());
+        for (Attribute<?, ?> attribute : attributes) {
+            boolean isId = idAttributeName != null && attribute.getName().equals(idAttributeName);
+            columns.add(createColumn(attribute, isId, tableSize));
+        }
+        HoloConfigTable table = new HoloConfigTable(tableName, writeable, tableSize, columns);
+        List<HoloConfigTable> tables = tablesBySchema.computeIfAbsent(schemaName, k -> new ArrayList<>());
+        tables.add(table);
+    }
+
+    private void sortAttributes(Class<?> entityClazz, List<Attribute<?, ?>> attributes) {
+        ImmutableList<String> orderedFieldNames = ImmutableList.of(entityClazz.getDeclaredFields()).map(Field::getName);
+        attributes.sort((a1, a2) -> Integer.compare(
+                findAttributeField(orderedFieldNames, a1),
+                findAttributeField(orderedFieldNames, a2)));
+    }
+    
+    private int findAttributeField(ImmutableList<String> orderedFieldNames, Attribute<?, ?> attribute) {
+        Member member = attribute.getJavaMember();
+        if (!(member instanceof AnnotatedElement)) {
+            return -1;
+        }
+        String fieldName = extractFieldName((AnnotatedElement) member);
+        return orderedFieldNames.indexOf(fieldName);
+    }
+
+    private HoloConfigColumn createColumn(Attribute<?, ?> attribute, boolean isId, BigInteger tableSize) {
+        Member member = attribute.getJavaMember();
+        if (!(member instanceof AnnotatedElement)) {
+            throw new IllegalArgumentException("Member type not supported: " + member.getClass());
+        }
+        AnnotatedElement annotatedMember = (AnnotatedElement) member;
+        String columnName = extractName(annotatedMember);
+        Class<?> type = extractType(annotatedMember);
+        boolean isNumber = Number.class.isAssignableFrom(type);
+        
+        // TODO
+        ColumnMode mode = (isId && isNumber) ? ColumnMode.COUNTER : ColumnMode.DEFAULT;
+        BigInteger nullCount = BigInteger.ZERO;
+        List<Object> values = new ArrayList<>();
+        String valuesBundle = null;
+        List<String> valuesForeignColumn = null;
+        
+        return new HoloConfigColumn(
+                columnName,
+                type,
+                mode,
+                nullCount,
+                values,
+                null,
+                valuesBundle,
+                null,
+                null,
+                null,
+                valuesForeignColumn);
+    }
+    
+    private String extractName(AnnotatedElement annotatedMember) {
+        Column columnAnnotation = annotatedMember.getAnnotation(Column.class);
+        if (columnAnnotation != null && !columnAnnotation.name().isEmpty()) {
+            return columnAnnotation.name();
+        } else {
+            return extractFieldName(annotatedMember);
+        }
+    }
+
+    private String extractFieldName(AnnotatedElement annotatedMember) {
+        if (annotatedMember instanceof Field) {
+            return ((Field) annotatedMember).getName();
+        } else if (annotatedMember instanceof Method) {
+            String memberName = ((Method) annotatedMember).getName();
+            Matcher matcher = getMethodPattern.matcher(memberName);
+            if (matcher.find()) {
+                return matcher.group(1).toLowerCase() + matcher.group(2);
+            } else {
+                return memberName;
+            }
+        } else {
+            throw new IllegalArgumentException("Member type not supported: " + annotatedMember.getClass());
+        }
+    }
+
+    private Class<?> extractType(AnnotatedElement annotatedMember) {
+        if (annotatedMember instanceof Field) {
+            return ((Field) annotatedMember).getType();
+        } else if (annotatedMember instanceof Method) {
+            return ((Method) annotatedMember).getReturnType();
+        } else {
+            throw new IllegalArgumentException("Member type not supported: " + annotatedMember.getClass());
+        }
+    }
+    
+}
