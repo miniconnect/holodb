40b2cafd6b1130acaea331969226a86eb55a8ad5
Dávid Horváth <horvath@webarticum.hu>
2025-02-07 19:25:13 +0100

    Add tests for laziness and caching in tree transformations

--- projects/regex/src/test/java/hu/webarticum/holodb/regex/algorithm/TreeSortingTransformerTest.java
+++ projects/regex/src/test/java/hu/webarticum/holodb/regex/algorithm/TreeSortingTransformerTest.java
@@ -33,24 +33,24 @@ class TreeSortingTransformerTest {
                 TreeNode.of(charClass("a"), ImmutableList.of(
                         TreeNode.of(charClass("b"), ImmutableList.of(
                                 TreeNode.of(charClass("c"), leafOnly))))),
-                        TreeNode.of(charClass("m"), ImmutableList.of(
-                                TreeNode.of(charClass("n"), leafOnly))),
-                        TreeNode.of(charClass("x"), ImmutableList.of(
-                                TreeNode.of(charClass("y"), ImmutableList.of(
-                                        leafNode,
-                                        TreeNode.of(charClass("z"), leafOnly)
+                TreeNode.of(charClass("m"), ImmutableList.of(
+                        TreeNode.of(charClass("n"), leafOnly))),
+                TreeNode.of(charClass("x"), ImmutableList.of(
+                        TreeNode.of(charClass("y"), ImmutableList.of(
+                                leafNode,
+                                TreeNode.of(charClass("z"), leafOnly)
         ))))));
         TreeNode resultNode = transformer.sort(rootNode);
         TreeNode expectedTreeNode = TreeNode.of(SpecialTreeValues.ROOT, ImmutableList.of(
                 TreeNode.of(charClass("a"), ImmutableList.of(
                         TreeNode.of(charClass("b"), ImmutableList.of(
                                 TreeNode.of(charClass("c"), leafOnly))))),
-                        TreeNode.of(charClass("m"), ImmutableList.of(
-                                TreeNode.of(charClass("n"), leafOnly))),
-                        TreeNode.of(charClass("x"), ImmutableList.of(
-                                TreeNode.of(charClass("y"), ImmutableList.of(
-                                        leafNode,
-                                        TreeNode.of(charClass("z"), leafOnly)
+                TreeNode.of(charClass("m"), ImmutableList.of(
+                        TreeNode.of(charClass("n"), leafOnly))),
+                TreeNode.of(charClass("x"), ImmutableList.of(
+                        TreeNode.of(charClass("y"), ImmutableList.of(
+                                leafNode,
+                                TreeNode.of(charClass("z"), leafOnly)
         ))))));
         assertThat(resultNode).isEqualTo(expectedTreeNode);
     }
@@ -176,6 +176,71 @@ class TreeSortingTransformerTest {
                                         TreeNode.of(charClass("a"), leafOnly)))))))
         ));
         assertThat(resultNode).isEqualTo(expectedTreeNode);
+    }
+
+    @Test
+    void testUnchangingLazyness() {
+        TreeSortingTransformer transformer = new TreeSortingTransformer();
+        TreeNode leafNode = TreeNode.of(SpecialTreeValues.LEAF, ImmutableList.empty());
+        ImmutableList<TreeNode> leafOnly = ImmutableList.of(leafNode);
+        TreeNode rootNode = TreeNode.of(SpecialTreeValues.ROOT, ImmutableList.of(
+                TreeNode.of(charClass("a"), ImmutableList.of(
+                        TreeNode.of(charClass("b"), ImmutableList.of(
+                                TreeNode.of(charClass("c"), leafOnly))))),
+                TreeNode.of(charClass("m"), ImmutableList.of(
+                        TreeNode.of(charClass("n"), leafOnly))),
+                TreeNode.of(charClass("x"), ImmutableList.of(
+                        TreeNode.of(charClass("y"), ImmutableList.of(
+                                leafNode,
+                                TreeNode.of(charClass("z"), leafOnly)
+        ))))));
+        TreeNode resultNode = transformer.sort(rootNode);
+        assertThat(resultNode).isSameAs(rootNode);
+    }
+
+    @Test
+    void testSubLaziness() {
+        TreeSortingTransformer transformer = new TreeSortingTransformer();
+        TreeNode leafNode = TreeNode.of(SpecialTreeValues.LEAF, ImmutableList.empty());
+        ImmutableList<TreeNode> leafOnly = ImmutableList.of(leafNode);
+        TreeNode subNode = TreeNode.of(charClass("abc"), ImmutableList.of(
+                TreeNode.of(charClass("g"), ImmutableList.of(
+                        leafNode,
+                        TreeNode.of(charClass("x"), leafOnly),
+                        TreeNode.of(charClass("y"), leafOnly),
+                        TreeNode.of(charClass("z"), leafOnly)))
+        ));
+        TreeNode rootNode = TreeNode.of(SpecialTreeValues.ROOT, ImmutableList.of(
+                subNode,
+                leafNode));
+        TreeNode resultNode = transformer.sort(rootNode);
+        assertThat(resultNode).isNotSameAs(rootNode).isNotEqualTo(rootNode);
+        TreeNode resultSecondChild = resultNode.children().get(1);
+        assertThat(resultSecondChild.value()).isEqualTo(charClass("abc"));
+        assertThat(resultSecondChild).isSameAs(subNode);
+    }
+
+    @Test
+    void testCaching() {
+        TreeSortingTransformer transformer = new TreeSortingTransformer();
+        TreeNode leafNode = TreeNode.of(SpecialTreeValues.LEAF, ImmutableList.empty());
+        ImmutableList<TreeNode> leafOnly = ImmutableList.of(leafNode);
+        TreeNode subNode = TreeNode.of(charClass("abc"), ImmutableList.of(
+                TreeNode.of(charClass("x"), leafOnly),
+                leafNode
+        ));
+        TreeNode rootNode = TreeNode.of(SpecialTreeValues.ROOT, ImmutableList.of(
+                TreeNode.of(charClass("z"), ImmutableList.of(subNode)),
+                subNode
+        ));
+        TreeNode resultNode = transformer.sort(rootNode);
+        assertThat(resultNode).isNotSameAs(rootNode).isNotEqualTo(rootNode);
+        TreeNode resultFirstSubNode = resultNode.children().get(0);
+        assertThat(resultFirstSubNode.value()).isEqualTo(charClass("abc"));
+        assertThat(resultFirstSubNode).isNotSameAs(subNode);
+        TreeNode resultSecondFirstSubSubNode = resultNode.children().get(1).children().get(0);
+        assertThat(resultSecondFirstSubSubNode.value()).isEqualTo(charClass("abc"));
+        assertThat(resultSecondFirstSubSubNode).isSameAs(resultFirstSubNode);
     }
     
     private CharClass charClass(String chars) {
--- projects/regex/src/test/java/hu/webarticum/holodb/regex/algorithm/TreeWeedingTransformerTest.java
+++ projects/regex/src/test/java/hu/webarticum/holodb/regex/algorithm/TreeWeedingTransformerTest.java
@@ -78,6 +78,69 @@ class TreeWeedingTransformerTest {
         assertThat(resultNodes).isEqualTo(expectedTreeNode);
     }
 
+    @Test
+    void testUnchangingLaziness() {
+        TreeWeedingTransformer transformer = new TreeWeedingTransformer();
+        TreeNode leafNode = TreeNode.of(SpecialTreeValues.LEAF, ImmutableList.empty());
+        ImmutableList<TreeNode> leafOnly = ImmutableList.of(leafNode);
+        TreeNode rootNode = TreeNode.of(SpecialTreeValues.ROOT, ImmutableList.of(
+                TreeNode.of(charClass("a"), ImmutableList.of(
+                        TreeNode.of(charClass("b"), ImmutableList.of(
+                                TreeNode.of(charClass("c"), leafOnly))))),
+                TreeNode.of(charClass("m"), ImmutableList.of(
+                        TreeNode.of(charClass("n"), leafOnly))),
+                TreeNode.of(charClass("x"), ImmutableList.of(
+                        TreeNode.of(charClass("y"), ImmutableList.of(
+                                leafNode,
+                                TreeNode.of(charClass("z"), leafOnly)
+        ))))));
+        TreeNode resultNode = transformer.weed(rootNode);
+        assertThat(resultNode).isSameAs(rootNode);
+    }
+
+    @Test
+    void testSubLaziness() {
+        TreeWeedingTransformer transformer = new TreeWeedingTransformer();
+        TreeNode leafNode = TreeNode.of(SpecialTreeValues.LEAF, ImmutableList.empty());
+        ImmutableList<TreeNode> leafOnly = ImmutableList.of(leafNode);
+        TreeNode subNode = TreeNode.of(charClass("abc"), ImmutableList.of(
+                TreeNode.of(charClass("x"), leafOnly),
+                TreeNode.of(charClass("y"), leafOnly),
+                TreeNode.of(charClass("z"), leafOnly)
+        ));
+        TreeNode rootNode = TreeNode.of(SpecialTreeValues.ROOT, ImmutableList.of(
+                TreeNode.of(null, leafOnly),
+                subNode));
+        TreeNode resultNode = transformer.weed(rootNode);
+        assertThat(resultNode).isNotSameAs(rootNode).isNotEqualTo(rootNode);
+        TreeNode resultSecondChild = resultNode.children().get(1);
+        assertThat(resultSecondChild.value()).isEqualTo(charClass("abc"));
+        assertThat(resultSecondChild).isSameAs(subNode);
+    }
+
+    @Test
+    void testCaching() {
+        TreeWeedingTransformer transformer = new TreeWeedingTransformer();
+        TreeNode leafNode = TreeNode.of(SpecialTreeValues.LEAF, ImmutableList.empty());
+        ImmutableList<TreeNode> leafOnly = ImmutableList.of(leafNode);
+        TreeNode subNode = TreeNode.of(charClass("abc"), ImmutableList.of(
+                TreeNode.of(null, leafOnly),
+                TreeNode.of(charClass("x"), leafOnly)
+        ));
+        TreeNode rootNode = TreeNode.of(SpecialTreeValues.ROOT, ImmutableList.of(
+                TreeNode.of(null, leafOnly),
+                TreeNode.of(charClass("r"), ImmutableList.of(subNode)),
+                subNode));
+        TreeNode resultNode = transformer.weed(rootNode);
+        assertThat(resultNode).isNotSameAs(rootNode).isNotEqualTo(rootNode);
+        TreeNode resultSecondFirstSubSubChild = resultNode.children().get(1).children().get(0);
+        assertThat(resultSecondFirstSubSubChild.value()).isEqualTo(charClass("abc"));
+        assertThat(resultSecondFirstSubSubChild).isNotSameAs(subNode);
+        TreeNode resultThirdSubChild = resultNode.children().get(2);
+        assertThat(resultThirdSubChild.value()).isEqualTo(charClass("abc"));
+        assertThat(resultThirdSubChild).isSameAs(resultSecondFirstSubSubChild);
+    }
+
     private CharClass charClass(String chars) {
         return CharClass.of(chars, charComparator);
     }
