2d51ca0912950f2db026d61cbe59bb38c317df5a
Dávid Horváth <horvath@webarticum.hu>
2025-03-02 05:38:26 +0100

    Simplify the config json schema generator task

--- projects/config/src/schemaGen/java/hu/webarticum/holodb/config/gradle/schemagen/SchemaGeneratorMain.java
+++ projects/config/src/schemaGen/java/hu/webarticum/holodb/config/gradle/schemagen/SchemaGeneratorMain.java
@@ -9,18 +9,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
 
+import hu.webarticum.holodb.config.HoloConfig;
 import hu.webarticum.miniconnect.lang.jackson.JacksonSupport;
 
 public class SchemaGeneratorMain {
 
-    public static void main(String[] args) throws ClassNotFoundException, IOException {
-        String className = args[0];
-        String outputPath = args[1];
+    public static void main(String[] args) throws IOException {
+        String outputPath = args[0];
         
-        Class<?> configClass = SchemaGeneratorMain.class.getClassLoader().loadClass(className);
         ObjectMapper mapper = JacksonSupport.createMapper();
         JsonSchemaGenerator generator = new JsonSchemaGenerator(mapper);
-        JsonSchema schema = generator.generateSchema(configClass);
+        JsonSchema schema = generator.generateSchema(HoloConfig.class);
         
         File schemaOutFile = new File(outputPath);
         File schemaOutDirectory = schemaOutFile.getParentFile();
