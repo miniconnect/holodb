42bbee95407dc70b26548d3a7af66fb48eb2308b
Dávid Horváth <horvath@webarticum.hu>
2025-07-26 13:57:10 +0200

    Clean class imports

--- projects/config/src/schemaGen/java/hu/webarticum/holodb/config/gradle/schemagen/SchemaGeneratorMain.java
+++ projects/config/src/schemaGen/java/hu/webarticum/holodb/config/gradle/schemagen/SchemaGeneratorMain.java
@@ -4,10 +4,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Supplier;
 
 import com.fasterxml.jackson.databind.JsonNode;
