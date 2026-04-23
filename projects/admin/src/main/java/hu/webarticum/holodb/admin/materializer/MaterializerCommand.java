9189b453d30716ccceb981769daa3846d9a855eb
Dávid Horváth <horvath@webarticum.hu>
2026-04-23 08:50:18 +0200

    Implement the connection part for the materialize command in admin

--- projects/admin/src/main/java/hu/webarticum/holodb/admin/materializer/MaterializerCommand.java
+++ projects/admin/src/main/java/hu/webarticum/holodb/admin/materializer/MaterializerCommand.java
@@ -1,7 +1,12 @@
 package hu.webarticum.holodb.admin.materializer;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import jakarta.inject.Singleton;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 @Singleton
 @Command(
@@ -11,12 +16,36 @@ import picocli.CommandLine.Command;
 )
 public class MaterializerCommand implements Runnable {
 
+    @Option(
+            names = { "-j", "--jdbc-url" },
+            description = "JDBC URL to connect to",
+            required = true)
+    private String jdbcUrl;
+
+    @Option(
+            names = { "-u", "--username" },
+            description = "Database username")
+    private String username;
+
+    @Option(
+            names = { "-p", "--password" }, 
+            description = "Database password",
+            prompt = "Password: ",
+            interactive = true, 
+            arity = "0..1")
+    private String password;
+
     @Override
     public void run() {
-        
-        // TODO
-        System.out.println("Materialize!");
-        
+        try (Connection connection = connect()) {
+            System.out.println(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Connection connect() throws SQLException {
+        return DriverManager.getConnection(jdbcUrl, username, password);
     }
 
 }
