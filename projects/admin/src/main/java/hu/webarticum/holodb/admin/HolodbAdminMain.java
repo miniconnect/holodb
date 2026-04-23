93ce2307e65934d7ea7707b5351515e9e76f9b9c
Dávid Horváth <horvath@webarticum.hu>
2026-04-23 09:21:01 +0200

    Merge the root command into the main class in the admin project

--- projects/admin/src/main/java/hu/webarticum/holodb/admin/HolodbAdminMain.java
+++ projects/admin/src/main/java/hu/webarticum/holodb/admin/HolodbAdminMain.java
@@ -1,12 +1,28 @@
 package hu.webarticum.holodb.admin;
 
+import hu.webarticum.holodb.admin.materializer.MaterializerCommand;
+import hu.webarticum.holodb.admin.buildconfig.BuildConfig;
 import picocli.CommandLine;
+import picocli.CommandLine.Command;
 
-public class HolodbAdminMain {
+@Command(
+        name = "holodb-admin",
+        description = BuildConfig.APP_DESCRIPTION,
+        version = BuildConfig.APP_VERSION,
+        mixinStandardHelpOptions = true,
+        subcommandsRepeatable = false,
+        subcommands = { MaterializerCommand.class }
+)
+public class HolodbAdminMain implements Runnable {
 
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new ApplicationCommand()).execute(args);
+        int exitCode = new CommandLine(new HolodbAdminMain()).execute(args);
         System.exit(exitCode); 
+    }
+
+    @Override
+    public void run() {
+        new CommandLine(this).usage(System.out);
     }
 
 }
